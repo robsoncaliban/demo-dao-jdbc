ef8fc05f352afc4c183c611fb049865a81ccea49
Robson Batista <86684265+robsoncaliban@users.noreply.github.com>
2023-08-11 18:08:13 -0300

    adição das classes 'Seller' e 'Department'

--- src/main/java/com/dao_jdbc/application/Main.java
+++ src/main/java/com/dao_jdbc/application/Main.java
@@ -1,10 +1,16 @@
 package com.dao_jdbc.application;
 
-import com.dao_jdbc.db.DB;
+import java.time.LocalDate;
+
+import com.dao_jdbc.model.Department;
+import com.dao_jdbc.model.Seller;
 
 public class Main {
     public static void main(String[] args) {
-        DB.getConnection();
-        DB.closeConnection();
+        Department obj = new Department(1, "Books");
+        LocalDate date = LocalDate.parse("2004-02-28");
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", date , 3000.0, obj);
+        System.out.println(seller);
     }
 }
