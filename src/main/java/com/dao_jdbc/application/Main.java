186dc0f9184c896a5b9b9d88e9812dd12418a87c
Robson Batista <86684265+robsoncaliban@users.noreply.github.com>
2023-08-14 17:43:43 -0300

    métodos 'update' e 'deleteById' implementados

--- src/main/java/com/dao_jdbc/application/Main.java
+++ src/main/java/com/dao_jdbc/application/Main.java
@@ -3,14 +3,17 @@ package com.dao_jdbc.application;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 
 import com.dao_jdbc.dao.DaoFactory;
 import com.dao_jdbc.dao.SellerDao;
+import com.dao_jdbc.db.DbException;
 import com.dao_jdbc.model.Department;
 import com.dao_jdbc.model.Seller;
 
 public class Main {
     public static void main(String[] args) {
+       
         SellerDao sellerDao = DaoFactory.creatSellerDao();
 
         Seller seller = sellerDao.findById(3);
@@ -30,6 +33,32 @@ public class Main {
         System.out.println(newSeller.getId());
 
         System.out.println();
+
+        newSeller.setName("Martha Waine");
+        newSeller.setEmail("marthawaine@gmail.com");
+        sellerDao.update(newSeller);
+
+        System.out.println();
+
+        Scanner sc = new Scanner(System.in);
+        boolean sellerDeleted = false;
+        
+        while(!sellerDeleted){
+            System.out.print("Enter id for delete: ");
+            int id = Integer.parseInt(sc.nextLine());
+
+            try {
+                sellerDao.deleteById(id);
+                sellerDeleted = true;
+                System.out.println("\nsuccessfully deleted seller");
+            } catch (DbException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+        sc.close();
+        System.out.println();
+
         list = sellerDao.findAll();
         list.forEach(System.out::println);
 
--- src/main/java/com/dao_jdbc/dao_impl/SellerDaoJDBC.java
+++ src/main/java/com/dao_jdbc/dao_impl/SellerDaoJDBC.java
@@ -62,15 +62,46 @@ public class SellerDaoJDBC implements SellerDao{
     }
 
     @Override
-    public void update(Seller obj) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    public void update(Seller seller) {
+         PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                "UPDATE seller "
+                + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+                + "WHERE Id = ? ",
+                Statement.RETURN_GENERATED_KEYS);
+
+            st.setString(1, seller.getName());
+            st.setString(2, seller.getEmail());
+            st.setDate(3, Date.valueOf(seller.getBirthDate()));
+            st.setDouble(4, seller.getBaseSalary());
+            st.setInt(5, seller.getDepartment().getId());
+            st.setInt(6, seller.getId());
+
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally{
+            DB.closeStatement(st);
+        }
     }
 
     @Override
     public void deleteById(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+        PreparedStatement st = null;
+
+        try {
+            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+            st.setInt(1, id);
+            int rowsAffected = st.executeUpdate();
+            if(rowsAffected == 0){
+                throw new DbException("id not found");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally{
+            DB.closeStatement(st);
+        }
     }
 
     @Override
